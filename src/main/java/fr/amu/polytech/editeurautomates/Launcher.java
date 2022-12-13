5f82d17e8711ca3660ed89448f8db6c04f4347df
Magyar57 <alxbrp@gmail.com>
2022-12-13 16:39:40 +0100

    Added debug mains for Model and Controller

--- src/main/java/fr/amu/polytech/editeurautomates/Launcher.java
+++ src/main/java/fr/amu/polytech/editeurautomates/Launcher.java
@@ -1,7 +1,32 @@
 package fr.amu.polytech.editeurautomates;
 
+import java.util.Objects;
+
 public class Launcher {
+
     public static void main(String[] args) {
+
+        // Debug functions
+        if (args.length > 1 && (Objects.equals(args[1], "--debugModel") || Objects.equals(args[1], "-dm"))){
+            debugModel();
+            return;
+        }
+        if (args.length > 1 && (Objects.equals(args[1], "--debugController") || Objects.equals(args[1], "-dc"))){
+            debugController();
+            return;
+        }
+
+        // Default main
         HelloApplication.main(args);
+
     }
+
+    private static void debugModel(){
+        System.out.println("debugModel");
+    }
+
+    private static void debugController(){
+        System.out.println("debugController");
+    }
+
 }
