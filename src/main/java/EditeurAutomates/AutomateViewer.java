e43c228b955ff7703b2f8904723e5459ec328ec2
Magyar57 <alxbrp@gmail.com>
2023-01-05 15:21:50 +0100

    Ajout d'une icône d'application

--- src/main/java/EditeurAutomates/AutomateViewer.java
+++ src/main/java/EditeurAutomates/AutomateViewer.java
@@ -3,21 +3,31 @@ package EditeurAutomates;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class AutomateViewer extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AutomateViewer.class.getResource("view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
 
-    public static void main(String[] args) {
-        launch();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(AutomateViewer.class.getResource("view/hello-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+		// Titre de la fenêtre
+		stage.setTitle("AutomatesLab");
+
+		// Icône de la fenêtre
+		String icon_name = Objects.requireNonNull(AutomateViewer.class.getResource("Images/Icon.png")).toString();
+		stage.getIcons().add(new Image(icon_name));
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 }
--- src/main/java/EditeurAutomates/Launcher.java
+++ src/main/java/EditeurAutomates/Launcher.java
@@ -1,31 +1,32 @@
 package EditeurAutomates;
 
-import java.util.Objects;
+import java.util.Set;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // Debug functions
-        if (args.length > 1 && (Objects.equals(args[1], "--debugModel") || Objects.equals(args[1], "-dm"))){
-            debugModel();
-            return;
-        }
-        if (args.length > 1 && (Objects.equals(args[1], "--debugController") || Objects.equals(args[1], "-dc"))){
-            debugController();
-            return;
-        }
+		// Debug functions
+		Set<String> arguments = Set.of(args);
+		if (arguments.contains("--debugModel") || arguments.contains("-dm")){
+			debugModel();
+			return;
+		}
+		if (arguments.contains("--debugController") || arguments.contains("-dc")){
+			debugController();
+			return;
+		}
 
-        // Default: launch main view
-        AutomateViewer.main(args);
-    }
+		// Default: launch main view
+		AutomateViewer.main(args);
+	}
 
-    private static void debugModel(){
-        System.out.println("debugModel");
-    }
+	private static void debugModel(){
+		System.out.println("debugModel");
+	}
 
-    private static void debugController(){
-        System.out.println("debugController");
-    }
+	private static void debugController(){
+		System.out.println("debugController");
+	}
 
 }
