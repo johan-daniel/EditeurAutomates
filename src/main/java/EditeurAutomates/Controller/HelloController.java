0e4192745e5f46ef5ea31cd387b05b43006dd195
Magyar57 <alxbrp@gmail.com>
2023-01-06 09:48:29 +0100

    Menu bar adaptative à l'OS ( macOS)

--- src/main/java/EditeurAutomates/Controller/HelloController.java
+++ src/main/java/EditeurAutomates/Controller/HelloController.java
@@ -2,10 +2,22 @@ package EditeurAutomates.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
 
 public class HelloController {
+    private final boolean isMacos;
+    @FXML private Label welcomeText;
+    @FXML private MenuBar mainMenuBar;
+
+    public HelloController() {
+        final String os = System.getProperty("os.name");
+        this.isMacos = (os != null && os.startsWith("Mac"));
+    }
+
     @FXML
-    private Label welcomeText;
+    public void initialize() {
+        if (isMacos) mainMenuBar.setUseSystemMenuBar(true);
+    }
 
     @FXML
     protected void onHelloButtonClick() {
