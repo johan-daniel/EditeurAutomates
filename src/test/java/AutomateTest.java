439883d06ed1ba39ef803701c3442c1738f202ec
johan-daniel <johan.daniel.perso@gmail.com>
2023-01-24 14:54:56 +0100

    avancer tests unitaires

--- src/test/java/AutomateTest.java
+++ src/test/java/AutomateTest.java
@@ -1,4 +1,5 @@
 import EditeurAutomates.Model.Automate;
+import EditeurAutomates.Model.State;
 import org.junit.jupiter.api.Test;
 public class AutomateTest {
     private Automate automate;
@@ -13,10 +14,24 @@ public class AutomateTest {
     @Test
     public void testCreateState() {
         automate = new Automate();
+
         // Regular state
         automate.createState(10, 10);
         assert(automate.getStatesList().size() == 1);
         assert(automate.getStatesList().get(0).numero == 0);
+
+        // Initial state
+        automate.createState(10, 10, true, false);
+        assert(automate.getStatesList().size() == 2);
+        State secondState =  automate.getStatesList().get(1);
+        assert(secondState.numero == 1);
+        assert(secondState.isInitial);
+
+        // Final state
+        automate.createState(10, 10, false, true);
+        assert(automate.getStatesList().size() == 3);
+        assert(automate.getStatesList().get(2).numero == 2);
+        assert(automate.getStatesList().get(2).isFinal);
     }
 
 }
