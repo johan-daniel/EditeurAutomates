14e4359b730f50d9e275b334cb04e47507e49550
Magyar57 <alxbrp@gmail.com>
2023-01-29 18:05:47 +0100

    Début de la forme de la vue XML

--- src/main/java/EditeurAutomates/Controller/XMLController.java
+++ src/main/java/EditeurAutomates/Controller/XMLController.java
@@ -1,6 +1,10 @@
 package EditeurAutomates.Controller;
 
+import javafx.scene.control.TextArea;
+
 public class XMLController extends ViewController {
+
+	public TextArea editor;
 
 	@Override
 	public void updateModel() {
@@ -10,6 +14,7 @@ public class XMLController extends ViewController {
 	@Override
 	public void pullModel() {
 		System.out.println("Pulling model");
+		//		editor.replaceText(0, 0, "Salut"); // a tester
 	}
 
 }
